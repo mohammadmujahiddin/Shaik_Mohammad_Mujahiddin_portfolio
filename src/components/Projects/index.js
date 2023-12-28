f7eb5f5b24ece6f8f0e0a5f733857655a1883153
mohammadmujahiddingit config --global user.email mohammadmujahiddin01@outlook.com <mohammadmujahiddin01@outlook.com>
2023-12-28 22:26:39 +0530

    Changes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -33,11 +33,11 @@ const Projects = ({openModal,setOpenModal}) => {
             <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
           }
           <Divider />
-          {/* {toggle === 'machine learning' ?
+          {toggle === 'machine learning' ?
             <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
             :
             <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
-          } */}
+          }
         </ToggleButtonGroup>
         <CardContainer>
           {toggle === 'all' && projects
